432d16f758d9d4a23b8ce04a127d4c18d188ad25
mollexmax048 <mollexmax048@gmail.com>
2024-10-11 13:19:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,10 +24,10 @@
 //*
 //base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
-//Instagram: ahmmikun
-//Telegram: t.me/ahmmitech
-//GitHub: @salmanytofficial
-//WhatsApp: +923184070915
+//Instagram: https://www.instagram.com/new_moe048?igsh=YnNpcnh1ZTJrZzFv&utm_source=qr
+//Telegram: https://t.me/moee048
+//GitHub: @mollexmax048
+//WhatsApp: +14143066181
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 //   * Created By Github: DGXeon.
 //   * Credit To Xeon
@@ -40,30 +40,30 @@ const chalk = require('chalk');
 
 //owmner v card
 global.ytname = "YT: S4 SALMAN YT" //ur yt chanel name
-global.socialm = "GitHub: salmanytofficial" //ur github or insta name
-global.location = "Pakistan, Lahore, Sabzazar" //ur location
+global.socialm = "GitHub: mollexmax048" //ur github or insta name
+global.location = "Tanzania, Mbezi, Dare'salaama" //ur location
 
 //new
-global.botname = 'XLICON-V4-MD' //ur bot name
-global.ownernumber = ['923184070915'] //ur owner number, dont add more than one
-global.ownername = '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙' //ur owner name
+global.botname = 'MOEVIPPEER-V1-MD' //ur bot name
+global.ownernumber = ['14143066181'] //ur owner number, dont add more than one
+global.ownername = '`【 ▬▬▬[ MǑẼ100VÏÏPPËÊR% ]▬▬▬ 】`' //ur owner name
 global.websitex = "https://youtube.com/@s4salmanyt"
 global.wagc = "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
 global.themeemoji = '⛩'
 global.wm = "Xlicon Bot Inc."
 global.botscript = 'https://github.com/salmanytofficial/XLICON-V4-MD' //script link
 global.packname = "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉"
-global.author = "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
-global.creator = "923184070915@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["923184070915"] // Premium User
+global.author = "▬▬▬[ MǑẼ100VÏÏPPËÊR% ]▬▬▬"
+global.creator = "14143066181@s.whatsapp.net"
+global.xprefix = ['','!','.','?']
+global.premium = ["14143066181"] // Premium User
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
 global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.antidelete = false //set anti delete 
+global.antidelete = true //set anti delete 
 
 
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
